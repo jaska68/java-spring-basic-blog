484f90fbfd3213653fccd3173bc61a8b42aca331
jaska68 <30998916+jaska68@users.noreply.github.com>
2020-08-12 12:43:27 +0300

    Module 2 changes

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,32 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.pluralsight.blog.model.Post;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
+
+    private PostRepository postRepository;
+    //private List<Post> posts;
+
+    public BlogController(PostRepository pr){
+        this.postRepository = pr;
+    }
+
+    public List<Post> listPosts(){
+        List<Post> posts = postRepository.getAllPosts();
+        return posts;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap mm){
-        mm.put("title", "Blog Post 1");
+        //mm.put("title", "Blog Post 1");
+        mm.put("posts", listPosts());
         return "home";
     }
 
